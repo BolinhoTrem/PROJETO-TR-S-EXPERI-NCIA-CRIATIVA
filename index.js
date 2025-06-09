fcf2361f185d3dae221c6149db9df1aff45080a1
BolinhoTrem <gabealves0310@gmail.com>
2025-06-09 08:50:29 -0300

    meu primeiro commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const inputtext = document.getElementsById("text");
+const inputpassword = document.getElementById("password");
+const button = document.getElementById("button")
+
+button.addEventListener("click", buttonclick);
+function buttonclick(){
+  let nome = inputtext.value;
+  let senha = inputpassword.value;
+  
+
+}
